ad9ee3522a0ae88b6a67f4f7827e199d72484125
blueal <blueal22@hotmail.com>
2014-03-05 16:04:52 -0800

    Version 0.5.0

--- extension.js
+++ extension.js
@@ -1,5 +1,22 @@
+//Lines 2-15 translates the HTML for the extension
+var _InputText = chrome.i18n.getMessage("InputText_html");
+var _SearchButton = chrome.i18n.getMessage("SearchButton_html");
+
+
+function translateHTML() {
+	document.getElementById("disclaimerSpan").innerHTML = chrome.i18n.getMessage("disclaimer_html"); //Immediately translates disclaimer
+	
+	var element1 = document.getElementById("InputText"); //Identifies the text box and defines it
+	element1.setAttribute('placeholder', _InputText); //sets the text box to a variable that is defined on line 2
+	
+	var element2 = document.getElementById("SearchButton"); //Identifies the button and defines it
+	element2.setAttribute('Value', _SearchButton); //sets the button text to a variable that is defined on line 3
+}
+window.onload = translateHTML; //This will immediately run the above function
+
+
+//Everything below this line is for Google Analytics
 var _AnalyticsCode = 'UA-41404176-1';
-	 	 	
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', _AnalyticsCode]);
